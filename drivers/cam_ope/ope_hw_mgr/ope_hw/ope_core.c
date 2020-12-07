7abd11634a224ebed05bae26068250b2b7143dcb
Yash Upadhyay <quic_yupadhya@quicinc.com>
2020-12-07 15:13:39 -0800

    msm: camera: common: Add cam_free_clear to cam_compat
    Kernel 5.10 and above have renamed kzfree to kfree_secure. As such,
    we have added a cam_free_clear function to redirect all uses of
    kzfree uses to the appropriate function depending on the kernel
    version the driver is built against.
    
    CRs-Fixed: 3514025
    Change-Id: I217034575ea52b025ff33ffcb5bc4c66ead78f44
    Signed-off-by: Yash Upadhyay <quic_yupadhya@quicinc.com>

--- drivers/cam_ope/ope_hw_mgr/ope_hw/ope_core.c
+++ drivers/cam_ope/ope_hw_mgr/ope_hw/ope_core.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2019-2021, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #include <linux/of.h>
@@ -13,6 +14,7 @@
 #include <linux/timer.h>
 #include <linux/iopoll.h>
 #include <media/cam_ope.h>
+
 #include "cam_io_util.h"
 #include "cam_hw.h"
 #include "cam_hw_intf.h"
@@ -27,6 +29,7 @@
 #include "cam_cdm_util.h"
 #include "ope_bus_rd.h"
 #include "ope_bus_wr.h"
+#include "cam_compat.h"
 
 static int cam_ope_caps_vote(struct cam_ope_device_core_info *core_info,
 	struct cam_ope_dev_bw_update *cpas_vote)
@@ -250,7 +253,7 @@ enable_soc_resource_failed:
 	else
 		core_info->cpas_start = false;
 free_cpas_vote:
-	kzfree(cpas_vote);
+	cam_free_clear((void *)cpas_vote);
 	cpas_vote = NULL;
 end:
 	return rc;
