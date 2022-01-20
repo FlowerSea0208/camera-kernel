e3ed9691c3be172d97b3690ec2516406fd1edd31
Hemant Yadav <quic_hemanty@quicinc.com>
2022-01-20 10:40:22 +0530

    msm:camera:sensor: Add support to update firmware
    - Add support to update firmware for HDMI bdg chip
    - Need to load firmware in the device separately
    
    Change-Id: I5ef0b564cb6f020bf90e97e72b43e8963c817a52
    Signed-off-by: Hemant Yadav <quic_hemanty@quicinc.com>

--- drivers/cam_sensor_module/cam_hdmi_bdg/cam_hdmi_bdg_core.h
+++ drivers/cam_sensor_module/cam_hdmi_bdg/cam_hdmi_bdg_core.h
@@ -1,13 +1,22 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
- * Copyright (c) 2021 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2021-2022 Qualcomm Innovation Center, Inc. All rights reserved.
  */
+#ifndef _CAMHDMIBDGCORE_H_
+#define _CAMHDMIBDGCORE_H_
+
+#include <linux/firmware.h>
 #include "cam_sensor_dev.h"
 
 #define HDMI_BDG_SENSOR_ID 0x1704
-#define HDMI_BDG_FW_VERSION 0x000001
 #define HDMI_BDG_HDMI_CONNECTED    0x55
 #define HDMI_BDG_HDMI_DISCONNECTED 0x88
+
+
+/**
+ * This API upgrade lt6911 firmware.
+ */
+int cam_hdmi_bdg_upgrade_firmware(void);
 
 /**
  * @s_ctrl: Sensor ctrl structure
@@ -23,3 +32,7 @@ void cam_hdmi_bdg_unset_cam_ctrl(void);
 
 int cam_hdmi_bdg_get_src_resolution(bool *signal_stable,
 		int *width, int *height, int *id);
+
+uint32_t cam_hdmi_bdg_get_fw_version(void);
+
+#endif /* _CAMHDMIBDGCORE_H_ */
