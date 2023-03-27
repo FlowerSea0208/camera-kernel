36ecba68d3ae19defbb945b6f5918681d66f30c6
Petar Sivenov <quic_c_psiven@quicinc.com>
2023-03-27 11:48:04 +0300

    msm: camera: isp: reconfigures IFE WMs for each frame
    Each offline IFE frame process is independent operation, so the WMs
    should be reconfigured for each frame as its dimensions can be
    different. All other parameters come as part of the request CDM packet
    and are applied by the CDM.
    
    CRs-Fixed: 3444135
    Signed-off-by: Petar Sivenov <quic_c_psiven@quicinc.com>
    Change-Id: I5b5bcc8a01267b6bcf4b28594ca553697e763c5c

--- drivers/cam_isp/isp_hw_mgr/isp_hw/include/cam_isp_hw.h
+++ drivers/cam_isp/isp_hw_mgr/isp_hw/include/cam_isp_hw.h
@@ -1,6 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
  * Copyright (c) 2017-2021, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #ifndef _CAM_ISP_HW_H_
@@ -113,6 +114,7 @@ enum cam_isp_hw_cmd_type {
 	CAM_ISP_HW_CMD_CSID_CHANGE_HALT_MODE,
 	CAM_ISP_HW_CMD_GET_IRQ_REGISTER_DUMP,
 	CAM_ISP_HW_CMD_CSID_CLOCK_DUMP,
+	CAM_ISP_HW_CMD_WM_UPDATE,
 	CAM_ISP_HW_CMD_SET_NUM_OF_ACQUIRED_RESOURCE,
 	CAM_ISP_HW_CMD_GET_NUM_OF_ACQUIRED_RESOURCE,
 	CAM_ISP_HW_CMD_MAX,
