6aa30cd2391dac5f2b88194061d7b6eda2c0cf17
Yash Upadhyay <quic_yupadhya@quicinc.com>
2023-07-26 11:24:12 +0530

    msm: camera: common: Enabling compilation support for holi
    Enabled compilation support for holi 2.5, by adding correct config mk
    target specific file.
    
    CRs-Fixed: 3514025
    Change-Id: Iec5fe8e143556a0f7f6267e130b06c4df3db9580
    Signed-off-by: Yash Upadhyay <quic_yupadhya@quicinc.com>

---
+++ config/holi.mk
@@ -0,0 +1,16 @@
+# SPDX-License-Identifier: GPL-2.0-only
+
+# Localized KCONFIG settings
+CONFIG_SPECTRA_ISP := y
+CONFIG_SPECTRA_OPE := y
+CONFIG_SPECTRA_TFE := y
+
+# Flags to pass into C preprocessor
+ccflags-y += -DCONFIG_SPECTRA_ISP=1
+ccflags-y += -DCONFIG_SPECTRA_TFE=1
+ccflags-y += -DCONFIG_SPECTRA_OPE=1
+
+# External Dependencies
+ifeq ($(CONFIG_QCOM_VA_MINIDUMP), y)
+KBUILD_CPPFLAGS += -DCONFIG_QCOM_VA_MINIDUMP=1
+endif
