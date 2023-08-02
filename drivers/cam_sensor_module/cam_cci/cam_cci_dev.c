dc405ca67234ed62d303333af3a1cfa3ddff109f
Vandana Jain <quic_vandjain@quicinc.com>
2023-08-02 12:30:06 +0530

    msm: reset cci-master for previous NACK
    In case of NACK in previous transactions, cci-master should be reset.
    
    Change-Id: Ia6b1c9003666ce29da8ac0e7c470e37dc3a98ef2
    Signed-off-by: Vandana Jain <quic_vandjain@quicinc.com>

--- drivers/cam_sensor_module/cam_cci/cam_cci_dev.c
+++ drivers/cam_sensor_module/cam_cci/cam_cci_dev.c
@@ -84,14 +84,16 @@ irqreturn_t cam_cci_irq(int irq_num, void *data)
 			cci_master_info = &cci_dev->cci_master_info[MASTER_0];
 			cci_dev->cci_master_info[MASTER_0].reset_pending =
 				FALSE;
-			if (!cci_master_info->status)
+			if (!cci_master_info->status ||
+					cci_master_info->status == -EINVAL)
 				complete(&cci_master_info->reset_complete);
 		}
 		if (cci_dev->cci_master_info[MASTER_1].reset_pending == TRUE) {
 			cci_master_info = &cci_dev->cci_master_info[MASTER_1];
 			cci_dev->cci_master_info[MASTER_1].reset_pending =
 				FALSE;
-			if (!cci_master_info->status)
+			if (!cci_master_info->status ||
+					cci_master_info->status == -EINVAL)
 				complete(&cci_master_info->reset_complete);
 		}
 	}
