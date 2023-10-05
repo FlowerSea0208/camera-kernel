c1ffe0d1aa26018acd648c5d03df6784496a8d06
Bharat Pawar <quic_bpawar@quicinc.com>
2023-10-05 10:36:14 +0530

    msm: camera: config: add support to compile out camera kernel module
    This change provides required support to disable camera module
    compilation along with all modules and supports module specific
    override to enable compilation if required.
    
    Change-Id: I81efeae1e3b72d63f32afe0e62ddbb7b4e1b7992
    Signed-off-by: Bharat Pawar <quic_bpawar@quicinc.com>

--- board.mk
+++ board.mk
@@ -1,6 +1,18 @@
 # Build camera kernel driver
+ENABLE_CAMERA_DLKM := false
+
+ifeq ($(TARGET_KERNEL_DLKM_DISABLE), true)
+ifeq ($(TARGET_KERNEL_DLKM_CAMERA_OVERRIDE), true)
+ENABLE_CAMERA_DLKM := true
+endif
+else
+ENABLE_CAMERA_DLKM := true
+endif
+
+ifeq ($(ENABLE_CAMERA_DLKM),true)
 ifneq ($(TARGET_BOARD_AUTO),true)
 ifeq ($(call is-board-platform-in-list,$(TARGET_BOARD_PLATFORM)),true)
 BOARD_VENDOR_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/camera.ko
+endif
 endif
 endif
--- product.mk
+++ product.mk
@@ -1,1 +1,13 @@
+ENABLE_CAMERA_DLKM := false
+
+ifeq ($(TARGET_KERNEL_DLKM_DISABLE), true)
+ifeq ($(TARGET_KERNEL_DLKM_CAMERA_OVERRIDE), true)
+ENABLE_CAMERA_DLKM := true
+endif
+else
+ENABLE_CAMERA_DLKM := true
+endif
+
+ifeq ($(ENABLE_CAMERA_DLKM), true)
 PRODUCT_PACKAGES += camera.ko
+endif
