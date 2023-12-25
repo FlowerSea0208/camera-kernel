e109d1288ec3caa86588a60ffc05a4b8afd67cb2
Rajat Goel <quic_rajgoe@quicinc.com>
2023-12-25 20:23:07 +0530

    msm: camera: sensor: Add support to write sensor registers in probe
    Few sensors needs to unlock register bank for reading
    sensor ID from sensor register for probe. And this change
    adds support to write sensor registers in probe cmd just
    after power on and before reading the sensor ID.
    
    CRs-Fixed: 3656392
    Change-Id: Id703cbde77b712d710281f54c5a6e29132e069a5
    Signed-off-by: Rajat Goel <quic_rajgoe@quicinc.com>

--- drivers/cam_sensor_module/cam_sensor/cam_sensor_dev.c
+++ drivers/cam_sensor_module/cam_sensor/cam_sensor_dev.c
@@ -217,6 +217,8 @@ static int32_t cam_sensor_driver_i2c_probe(struct i2c_client *client,
 	INIT_LIST_HEAD(&(s_ctrl->i2c_data.config_settings.list_head));
 	INIT_LIST_HEAD(&(s_ctrl->i2c_data.streamon_settings.list_head));
 	INIT_LIST_HEAD(&(s_ctrl->i2c_data.streamoff_settings.list_head));
+	INIT_LIST_HEAD(&(s_ctrl->i2c_data.reg_bank_unlock_settings.list_head));
+	INIT_LIST_HEAD(&(s_ctrl->i2c_data.reg_bank_lock_settings.list_head));
 	INIT_LIST_HEAD(&(s_ctrl->i2c_data.read_settings.list_head));
 
 	for (i = 0; i < MAX_PER_FRAME_ARRAY; i++) {
@@ -307,6 +309,8 @@ static int cam_sensor_component_bind(struct device *dev,
 	INIT_LIST_HEAD(&(s_ctrl->i2c_data.config_settings.list_head));
 	INIT_LIST_HEAD(&(s_ctrl->i2c_data.streamon_settings.list_head));
 	INIT_LIST_HEAD(&(s_ctrl->i2c_data.streamoff_settings.list_head));
+	INIT_LIST_HEAD(&(s_ctrl->i2c_data.reg_bank_unlock_settings.list_head));
+	INIT_LIST_HEAD(&(s_ctrl->i2c_data.reg_bank_lock_settings.list_head));
 	INIT_LIST_HEAD(&(s_ctrl->i2c_data.read_settings.list_head));
 
 	for (i = 0; i < MAX_PER_FRAME_ARRAY; i++) {
